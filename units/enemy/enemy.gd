9d82c53ac044899613ec3c5b1c58369381a79805
Samuel Alves Domínguez <samueladad@gmail.com>
2025-07-22 21:30:18 +0200

    Pre-rediseño

--- units/enemy/enemy.gd
+++ units/enemy/enemy.gd
@@ -11,7 +11,13 @@ enum STATES{
 
 @onready var current_state: STATES = STATES.ALIVE
 @onready var guarding: bool = false
+@onready var current_combo: Array = []
 @onready var battle_scene: BattleScene = get_tree().get_first_node_in_group("BattleScene")
+
+@onready var hp: Control = $EnemyHP
+@onready var combo_01_pos: Vector2 = $EnemyHP/Combo01.position
+@onready var combo_02_pos: Vector2 = $EnemyHP/Combo02.global_position
+@onready var combo_03_pos: Vector2 = $EnemyHP/Combo03.global_position
 
 @onready var enemy_stats: Dictionary = {
 	"max_hp" : 100,
@@ -38,8 +44,10 @@ func dead() -> void:
 	enemy_dead.emit(self)
 	
 func skill() -> void:
-	print("Una skill")
-
+	var total_damage: int = enemy_stats["attack"]
+	total_damage += ceil(check_combo(0))
+	PlayerStats.damage(total_damage)
+	add_combo(GlobalManager.combo_pieces[0])
 
 func damage(amount: int) -> void:
 	var total_amount: int = amount
@@ -67,4 +75,29 @@ func update_life(amount: int) -> void:
 		if damage_bar.value >= 0:
 			i = amount
 		await get_tree().create_timer(0.01).timeout
+
+func add_combo(piece) -> void:
+	var combo_piece = piece.instantiate()
+	current_combo.append(combo_piece)
+	hp.add_child(combo_piece)
+	match current_combo.size:
+		1:
+			combo_piece.global_postion = combo_01_pos
+		2:
+			combo_piece.global_postion = combo_02_pos
+		3:
+			combo_piece.global_postion = combo_02_pos
+			finisher()
 	
+
+func check_combo(piece_name: int) -> float:
+	var result: float
+	for combo: ComboPiece in current_combo:
+		if combo.piece_name == piece_name:
+			result += combo.passive()
+	return result
+
+func finisher() -> void:
+	for piece in current_combo:
+		piece.queue_free()
+	current_combo.clear()
